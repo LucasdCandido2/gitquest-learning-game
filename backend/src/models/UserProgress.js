384a565f962cd99994228fdca043e20b718296ce
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2025-12-12 14:19:03 -0300

    feat(lesson): create Lesson and UserProgress models for game content

---
+++ backend/src/models/UserProgress.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class UserProgress extends Model {
+        static associate(models) {
+            UserProgress.belongsTo(models.User, { foreignKey: 'userId' });
+            UserProgress.belongsTo(models.Lesson, { foreignKey: 'lessonId' });
+        }
+    }
+
+    UserProgress.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        lessonId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'lessons',
+                key: 'id',
+            },
+        },
+        isCompleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        completedAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    }, {
+        sequelize,
+        modelName: 'UserProgress',
+        tableName: 'user_progresses',
+    });
+
+    return UserProgress;
+};
